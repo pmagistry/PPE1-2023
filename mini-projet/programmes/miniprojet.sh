2ea14b4ae1049b2ed7cbbcf642c1e02fb89c84e8
Pierre M <pierre.magistry@inalco.fr>
2023-12-06 11:18:24 +0100

    ajout iconv

--- mini-projet/programmes/miniprojet.sh
+++ mini-projet/programmes/miniprojet.sh
@@ -15,20 +15,24 @@ then
 fi
 
 CIBLE="robots?"
-
-
 lineno=1
 while read -r URL
 do
   lang=$(basename $URLS .txt)
 	response=$(curl -s -L -w "%{http_code}" -o "./aspirations/${lang}-${lineno}.html" $URL)
-	encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
+  encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1 | tr "[:lower:]" "[:upper:]")
+
   COUNT=0
   TEXTFILE="NA"
   if [ $response -eq 200 ]
   then
+    if [ ! $encoding == "UTF-8" ]  
+    then
+      iconv -f "$encoding" -t "UTF-8" -o "/tmp/recode_${lineno}.html" "./aspirations/${lang}-${lineno}.html"
+      mv "/tmp/recode_${lineno}.html" "./aspirations/${lang}-${lineno}.html"
+    fi
     # création du dump text
-    lynx -dump -nolist ./aspirations/${lang}-${lineno}.html > ./dumps-text/${lang}-${lineno}.txt
+    lynx -assume_charset UTF-8 -dump -nolist ./aspirations/${lang}-${lineno}.html > ./dumps-text/${lang}-${lineno}.txt
     TEXTFILE="../dumps-text/${lang}-${lineno}.txt"
     COUNT=$(grep -P -i -o "$CIBLE" ./dumps-text/${lang}-${lineno}.txt | wc -l)
     grep -i -C 3 "$CIBLE"  ./dumps-text/${lang}-${lineno}.txt > ./contextes/${lang}-${lineno}.txt  
